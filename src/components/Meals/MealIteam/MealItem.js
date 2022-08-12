276f18724d3f46be2eafafca6b162f2602cf2e39
18-RAJAT <Rajatjoshi7535@gmail.com>
2022-08-12 18:19:53 +0530

    Header js

--- src/components/Meals/MealIteam/MealItem.js
+++ src/components/Meals/MealIteam/MealItem.js
@@ -1,8 +1,10 @@
 import classes from './MealItem.module.css';
 
 const MealItem = (props) => {
-    //1st $ sign is denote output character dollar sign
-    //2nd $ sign is denote curly braces to inject dynamic content into the template literal
+   /*
+        1st $ sign is denote output character dollar sign
+        2nd $ sign is denote curly braces to inject dynamic content into the template literal
+    */
   const price = `$${props.price.toFixed(2)}`;
 
   return (
@@ -12,7 +14,8 @@ const MealItem = (props) => {
         <div className={classes.description}>{props.description}</div>
         <div className={classes.price}>{price}</div>
       </div>
-      <div></div>
+      <div>
+      </div>
     </li>
   );
 };
