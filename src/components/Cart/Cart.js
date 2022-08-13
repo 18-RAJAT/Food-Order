855d6ff9139dc1b05d2c3af0904c50725c2355d5
18-RAJAT <Rajatjoshi7535@gmail.com>
2022-08-13 14:11:33 +0530

    Cart items

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,1 +1,14 @@
+import classes from './Cart.module.css';
 
+
+const Cart = props =>{
+    const cartItems=[{id:'c1',name:'Sushi',amount:2,price:12.95}].map(item=><li>{item.name}</li>);
+    return(
+        <div>
+            {cartItems}
+            <div></div>
+            <div></div>
+        </div>
+    );
+};
+export default Cart;
