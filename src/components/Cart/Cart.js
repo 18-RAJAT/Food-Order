6f000b85136393bb3ef5ac213892354aeff3d169
18-RAJAT <Rajatjoshi7535@gmail.com>
2022-08-13 14:14:34 +0530

    Cart items

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,14 +1,27 @@
 import classes from './Cart.module.css';
 
 
-const Cart = props =>{
-    const cartItems=[{id:'c1',name:'Sushi',amount:2,price:12.95}].map(item=><li>{item.name}</li>);
-    return(
-        <div>
-            {cartItems}
-            <div></div>
-            <div></div>
-        </div>
+const Cart = (props) => {
+    const cartItems = (
+      <ul className={classes['cart-items']}>
+        {[{ id: 'c1', name: 'Sushi', amount: 2, price: 12.99 }].map((item) => (
+          <li>{item.name}</li>
+        ))}
+      </ul>
     );
-};
-export default Cart;
+    return (
+        <div>
+          {cartItems}
+          <div className={classes.total}>
+            <span>Total Amount</span>
+            <span>35.62</span>
+          </div>
+          <div className={classes.actions}>
+            <button className={classes['button--alt']}>Close</button>
+            <button className={classes.button}>Order</button>
+          </div>
+        </div>
+      );
+    };
+    
+    export default Cart;
