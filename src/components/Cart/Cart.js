555779ab498ba58b1e3db80c36e568b0c87b8672
18-RAJAT <Rajatjoshi7535@gmail.com>
2022-08-13 15:46:32 +0530

    Function button

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -4,7 +4,7 @@ import Modal from '../UI/Model';
 const Cart = (props) => {
     const cartItems = (
       <ul className={classes['cart-items']}>
-        {[{ id: 'c1', name: 'Sushi', amount: 2, price: 12.99 }].map((item) => (
+        {[{ id: 'c1', name: 'Pizza', amount: 2, price: 12.99 }].map((item) => (
           <li>{item.name}</li>
         ))}
       </ul>
@@ -17,7 +17,7 @@ const Cart = (props) => {
             <span>35.62</span>
           </div>
           <div className={classes.actions}>
-            <button className={classes['button--alt']}>Close</button>
+            <button className={classes['button--alt']} onClick={props.onClose}>Close</button>
             <button className={classes.button}>Order</button>
           </div>
         </Modal>
