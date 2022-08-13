0f98ed7d7c9b1f218c6a49edd46e2f6489049c59
18-RAJAT <Rajatjoshi7535@gmail.com>
2022-08-13 15:25:44 +0530

    Header js

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -8,7 +8,7 @@ const Header = (props) => {
     return (
       <Fragment>
         <header className={classes.header}>
-          <h1>Hungry Point</h1>
+          <h1>Foodie we</h1>
           <HeaderCartButton />
         </header>
         <div className={classes['main-image']}>
