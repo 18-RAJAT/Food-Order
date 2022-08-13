51acee6b5c7f730fc8130810147ba5241e2fef78
18-RAJAT <Rajatjoshi7535@gmail.com>
2022-08-13 14:26:38 +0530

    model js

--- src/components/UI/Model.js
+++ src/components/UI/Model.js
@@ -1,6 +1,26 @@
 import classes from './Model.module.css';
+import { Fragment } from 'react';
+const Backdrop = props =>{
+    return(
+        <div className={classes.backdrop}/>
+    );
+};
+
+const ModelOverlay = props =>{
+    return(
+        <div className={classes.modal}>
+        <div className={classes.content}>{props.children}</div>
+        </div>
+    );
+};
+
 
 const Model = props =>{
-    
+    retun(
+        <Fragment>
+            <Backdrop/>
+            <ModelOverlay>{props.children}</ModelOverlay>
+        </Fragment>
+    );
 };
 export default Model;
