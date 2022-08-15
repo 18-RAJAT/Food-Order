1932e0f498836a99fbdd6bffbb1a520da5b4c00a
18-RAJAT <Rajatjoshi7535@gmail.com>
2022-08-15 14:37:27 +0530

    Update all

--- src/components/Meals/MealIteam/MealItemForm.js
+++ src/components/Meals/MealIteam/MealItemForm.js
@@ -1,10 +1,24 @@
+import { useRef ,useState} from 'react';
 import Input from '../../UI/Input';
 import classes from './MealItemForm.module.css';
 
 const MealItemForm = (props) => {
+  const amountInputRef = useRef();
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+    // props.onAddMeal(props.meal);
+    const enteredAmount = amountInputRef.current.value;
+    const enteredAmountNumber = +enteredAmount;
+
+    if(enteredAmount.trim().length === 0 || enteredAmountNumber <1 || enteredAmountNumber > 5) {
+      return;
+    } 
+  };
   return (
     <form className={classes.form}>
       <Input
+        ref={amountInputRef}
         label='Amount'
         input={{
         id: 'amount_' + props.id, // this changed!
