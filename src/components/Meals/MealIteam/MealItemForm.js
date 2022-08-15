73111d534c2cf52da5c2660b43362085d6ef5b5f
18-RAJAT <Rajatjoshi7535@gmail.com>
2022-08-15 14:58:14 +0530

    conditional randring in mealitem js

--- src/components/Meals/MealIteam/MealItemForm.js
+++ src/components/Meals/MealIteam/MealItemForm.js
@@ -3,17 +3,21 @@ import Input from '../../UI/Input';
 import classes from './MealItemForm.module.css';
 
 const MealItemForm = (props) => {
+  const [amountIsValid, setAmountIsValid] = useState(true);
   const amountInputRef = useRef();
 
   const submitHandler = (event) => {
     event.preventDefault();
     // props.onAddMeal(props.meal);
     const enteredAmount = amountInputRef.current.value;
-    const enteredAmountNumber = +enteredAmount; //string number to a number change 
+    const enteredAmountNumber = +enteredAmount; //string number to a number change the string to a number
 
     if(enteredAmount.trim().length === 0 || enteredAmountNumber <1 || enteredAmountNumber > 5) {
+      setAmountIsValid(false);
       return;
     } 
+
+    props.onAddToCart(enteredAmountNumber);
   };
   return (
     <form className={classes.form}>
@@ -30,6 +34,7 @@ const MealItemForm = (props) => {
     }}
 />
       <button>+ Add</button>
+      {!amountIsValid && <p>Please enter a valid amount(1-5)</p>}
     </form>
   );
 };
